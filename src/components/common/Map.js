4dc36949e40a19d4f77d2c58533c09d46a809f39
seb yeates <yeatessa@gmail.com>
2019-02-13 13:54:35 +0000

    Maps removed view gem from gem page in mapbox popup

--- src/components/common/Map.js
+++ src/components/common/Map.js
@@ -16,7 +16,7 @@ class Map extends React.Component {
     // MAP Component new map made
     this.map = new mapboxgl.Map({
       container: this.mapDiv,
-      style: 'mapbox://styles/mapbox/light-v9'
+      style: 'mapbox://styles/mapbox/light-v9',
     })
 
     // Add geolocate control to the map.
@@ -75,12 +75,19 @@ class Map extends React.Component {
               </div>
               <h4>${name}</h4>
               <a href="https://www.google.com/maps/dir/?api=1&origin=${lat},${lng}&destination=${location.lat},${location.lon}" target="_blank" > Directions </a>
-              <a href="http://localhost:8000/gems/${_id}">View Gem</a>
-            `
-          )
+            `)
       )
       if(this.props.type === 'gem'){
         this.popup.addTo(this.map)
+      }else{
+        this.popup.setHTML(`
+          <div class="event-image">
+          <img src="${image}" alt="${name}" />
+          </div>
+          <h4>${name}</h4>
+          <a href="https://www.google.com/maps/dir/?api=1&origin=${lat},${lng}&destination=${location.lat},${location.lon}" target="_blank" > Directions </a>
+          <a href="http://localhost:8000/gems/${_id}">View Gem</a>
+          `)
       }
     })
   }
