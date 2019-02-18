d0b84c4bd4b2a2184f84e311c7b9c7336dcca55f
seb yeates <yeatessa@gmail.com>
2019-02-18 19:02:35 +0000

    changed map.js

--- src/components/common/Map.js
+++ src/components/common/Map.js
@@ -49,7 +49,7 @@ class Map extends React.Component {
   }
 
   generatePopups() {
-    if(!this.props.userLocation) return false
+    // if(!this.props.userLocation) return false
     this.popupsGenerated = true
     const { lat, lng } = this.props.userLocation
 
