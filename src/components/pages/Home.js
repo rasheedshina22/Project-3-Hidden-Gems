b99a86f9382f73a2031d7de4ce1b91e8f8191224
seb yeates <yeatessa@gmail.com>
2019-02-18 16:50:47 +0000

    New home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
           <li></li>
         </ul>
         <Link className="hoverWrapper" to={'/trips'}><button className="button homesecond is-medium thumbnail"> Trips to Explore <i className="fas fa-map-signs homepage"></i> </button></Link>
-        <div className="hoverShow"><span> Explore remarkable trips . . . </span></div>
+        <div className="hoverShow"><span> Explore remarkable trips and Discover Hidden Gems. . . </span></div>
         <Link className="hoverWrapper" to = {'/gems'}><button className="button home is-medium"> Hidden Gems  <i className="far fa-gem homepage"></i>  </button></Link>
         <div className="hoverShow"><span> Discover Hidden Gems you did not know about . . . </span></div>
       </div>
